db94d6e890641e96e8b635eb00f1921c3ae89213
Jrz5220 <felixlazo94@gmail.com>
2024-09-07 11:11:37 -0400

    Finish module 2 tasks

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
 
+/* changed path='login' to path='register' and 
+element={<LoginPanel />} to element={<Register />}
+to render the register page when regsiter button is clicked. */
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
