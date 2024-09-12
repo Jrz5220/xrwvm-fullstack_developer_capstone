efbcc8a3d3a19a0ab777061a532435fc89145198
Jrz5220 <53059217+Jrz5220@users.noreply.github.com>
2024-09-11 21:31:24 -0400

    Configure route for logout view

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,6 +16,9 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
