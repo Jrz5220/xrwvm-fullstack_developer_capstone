cc39788e76c751b9a317119b52f8670c1f0a4e6a
Jrz5220 <53059217+Jrz5220@users.noreply.github.com>
2024-09-19 20:26:15 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -52,7 +52,7 @@ def analyze_review_sentiments(text):
 # Add code for posting review
 # will take a data dictionary in and call add_review in the backend.
 def post_review(data_dict):
-    request_url = backend_url + "/insert_review";
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
