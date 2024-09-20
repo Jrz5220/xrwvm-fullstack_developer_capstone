e6905cd04ebefbcf4eb86921cc7fbf0828663d4b
Jrz5220 <53059217+Jrz5220@users.noreply.github.com>
2024-09-19 20:45:42 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,10 +7,13 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # path to get list of cars (requires you to create a root user with Django admin to populate cars table for your Django app)
-    # if you try to go to /get_cars without initializing root user to populate the cars table, you will get an error stating "no such table"
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    
+    # path to get list of cars
+    # (requires you to create a root user with Django admin to
+    # populate cars table for your Django app)
+    # if you try to go to /get_cars without initializing root user to
+    # populate the cars table, you will get an error stating "no such table"
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+
     # path for registration
     path(route='register', view=views.registration, name='register'),
 
@@ -21,12 +24,32 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(
+        route='get_dealers/',
+        view=views.get_dealerships,
+        name='get_dealers'
+    ),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
 
     # path for add a review view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_details'
+    ),
+    path(
+        route='add_review',
+        view=views.add_review,
+        name='add_review'
+    ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
